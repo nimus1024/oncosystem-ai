7133d5a744a8ef166dfbe16956b723d5c940bcc4
Batina Shikhalieva <77186747+bs35@users.noreply.github.com>
2023-02-18 22:44:17 -0800

    Working call to model.predict

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from keras.models import load_model
-from keras.preprocessing import image
+import keras.utils as image
+from keras.applications.imagenet_utils import decode_predictions 
 
 app = Flask(__name__)
 
@@ -11,11 +12,19 @@ model = load_model('cancer_pretrained_model.h5')
 model.make_predict_function()
 
 def predict_label(img_path):
-	i = image.load_img(img_path, target_size=(100,100))
+	i = image.load_img(img_path, target_size=(224,224))
 	i = image.img_to_array(i)/255.0
-	i = i.reshape(1, 100,100,3)
-	p = model.predict_classes(i)
-	return dic[p[0]]
+	# NOTE: changed shape from 1, 100,100 to 224,224,3 per our model in 2_train_model
+	i = i.reshape(1, 224,224,3)
+	p = model.predict(i)
+	print(p)
+	if p[0][0] > p[0][1]:
+		return "benign"
+	else:
+		return "malignant"
+	# dec_pred = decode_predictions(p)
+	
+	# return dic[p[0]]
 
 
 # routes
@@ -32,7 +41,8 @@ def get_output():
 	if request.method == 'POST':
 		img = request.files['my_image']
 
-		img_path = "static/" + img.filename	
+		#img_path = "static/" + img.filename	
+		img_path = img.filename	
 		img.save(img_path)
 
 		p = predict_label(img_path)
