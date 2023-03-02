6b7f1c2ee6e4b23f928284e81b484d3960c83605
khshik <khshik777@gmail.com>
2023-03-02 10:01:48 +0300

    upd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -70,7 +70,7 @@ const Navbar = () => {
               alt="user-profile"
             />
             <p>
-              <span className="text-gray-400 text-14">Александр А.И.</span>{' '}
+              <span className="text-gray-400 text-14">Иванов А.И.</span>{' '}
               <span className="text-gray-400 font-bold ml-1 text-14">
               </span>
             </p>
