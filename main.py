d7dab219b7712915801dc039439fb87eeeea3440
nimus1024 <129117196+nimus1024@users.noreply.github.com>
2023-04-12 18:11:33 -0700

    Update main.py

--- main.py
+++ main.py
@@ -11,33 +11,19 @@ import gcsfs
 import os
 app = Flask(__name__)
 
+model = load_model('cancer_pretrained_model.h5')
 
-#os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "glowing-program-379304-00f032baaa6d.json"
-# PROJECT = "CANCER_TREATMENT"
-# REGION = "europe-west2"
+# GCloud hosting [instance out of memory]
+# PROJECT_NAME = 'web-app-cancer-treatment'
+# CREDENTIALS = 'web-app-cancer-treatment-90c0c8e4ff2d.json'
+# MODEL_PATH = 'gs://pretrained-melanoma-model/cancer_pretrained_model.h5'
 
-#app = Flask(__name__)
-
-# model = load_model('cancer_pretrained_model.h5')
-
-# PROJECT_NAME = 'glowing-program-379304'
-# CREDENTIALS = 'glowing-program-379304-27f18cfbab2c.json'
-# MODEL_PATH = 'gs://cancer_treatment/cancer_pretrained_model.h5'
-
-PROJECT_NAME = 'web-app-cancer-treatment'
-CREDENTIALS = 'web-app-cancer-treatment-90c0c8e4ff2d.json'
-MODEL_PATH = 'gs://pretrained-melanoma-model/cancer_pretrained_model.h5'
-
-FS = gcsfs.GCSFileSystem(project=PROJECT_NAME,
-                         token=CREDENTIALS)
-with FS.open(MODEL_PATH, 'rb') as model_file:
-     model_gcs = h5py.File(model_file, 'r')
-     model = load_model(model_gcs)
+# FS = gcsfs.GCSFileSystem(project=PROJECT_NAME,
+#                          token=CREDENTIALS)
+# with FS.open(MODEL_PATH, 'rb') as model_file:
+#      model_gcs = h5py.File(model_file, 'r')
+#      model = load_model(model_gcs)
 			   
-
-# get model
-# model = load_model("gs://cancer_treatment/cancer_pretrained_model.h5")
-
 model.make_predict_function()
 
 def predict_label(img_path):
@@ -52,11 +38,6 @@ def predict_label(img_path):
 		return ["доброкачественная", benign, malignant]
 	else:
 		return ["меланома", benign, malignant]
-
-# routes
-# @app.route("/", methods=['GET', 'POST'])
-# def main():
-# 	return render_template("index.html")
 
 @app.route("/")
 def about_page():
